367e195ba79212e88ff05d811fa326f74817f093
Salil03 <salilgokhale24@gmail.com>
2024-02-23 15:21:08 +0530

    added unit square mesh

--- a2/src/square.cpp
+++ a2/src/square.cpp
@@ -15,25 +15,43 @@ int main(int argc, char** argv)
         return EXIT_FAILURE;
     }
 
-    Mesh m;
-    std::vector<glm::vec3> vert_pos = {glm::vec3(-1, 1, 0), glm::vec3(1, 1, 0), glm::vec3(1, -1, 0),
-                                       glm::vec3(-1, -1, 0)};
-    std::vector<glm::vec3> vert_normals = {glm::vec3(0, 0, 1), glm::vec3(0, 0, 1), glm::vec3(0, 0, 1),
-                                           glm::vec3(0, 0, 1)};
-    m.set_vert_attribs(vert_pos, vert_normals);
-    std::vector<glm::ivec3> faces = {glm::ivec3(0, 3, 2), glm::ivec3(0, 2, 1)};
-    m.set_faces(faces);
+    Mesh mesh;
+    int m = 10, n = 10;
+    std::vector<glm::vec3> vert_pos;
+    std::vector<glm::vec3> vert_normals;
+    for (int i = 0; i <= n; i++)
+    {
+        for (int j = 0; j <= m; j++)
+        {
+            vert_pos.push_back(glm::vec3(-1 + 2 * i / float(n), -1 + 2 * j / float(m), -0.5));
+            vert_normals.push_back(glm::vec3(0, 0, 1));
+        }
+    }
+    std::vector<glm::ivec3> faces;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            faces.push_back(glm::ivec3(i * (m + 1) + j + 1, i * (m + 1) + j, (i + 1) * (m + 1) + j));
+            faces.push_back(glm::ivec3(i * (m + 1) + j + 1, (i + 1) * (m + 1) + j, (i + 1) * (m + 1) + j + 1));
+        }
+    }
 
-    std::cout << "Loaded " << m.n_verts << " verts, " << m.n_tris << " triangles and " << m.n_edges << " edges\n";
+    mesh.set_vert_attribs(vert_pos, vert_normals);
+
+    mesh.set_faces(faces);
+
+    std::cout << "Loaded " << mesh.n_verts << " verts, " << mesh.n_tris << " triangles and " << mesh.n_edges
+              << " edges\n";
 
     /*
     load_object(argv[1], vertices, normals, triangles);
     int n_verts = vertices.size();
     int n_tris = triangles.size();
     */
-    v.setVertices(m.n_verts, m.vert_pos.data());
-    v.setNormals(m.n_verts, m.vert_normal.data());
-    v.setTriangles(m.n_tris, m.tri_verts.data());
+    v.setVertices(mesh.n_verts, mesh.vert_pos.data());
+    v.setNormals(mesh.n_verts, mesh.vert_normal.data());
+    v.setTriangles(mesh.n_tris, mesh.tri_verts.data());
 
     v.view();
 }
