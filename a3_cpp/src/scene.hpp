3ba4392e176876b9507cfc246e1f64eb3bef567f
Salil03 <salilgokhale24@gmail.com>
2024-03-25 17:14:45 +0530

    compiles on ubuntu

--- a3_cpp/src/scene.hpp
+++ a3_cpp/src/scene.hpp
@@ -4,15 +4,17 @@
 #include <vector>
 #include "camera.hpp"
 #include "object.hpp"
+#include <functional>
 
-class Scene {
-    public:
+class Scene
+{
+  public:
     int w, h;
     Camera camera;
     std::vector<std::reference_wrapper<Object>> objects;
 
     Scene(int _w, int _h);
     Ray generate_ray(int px, int py);
-    glm::vec4 trace_ray(Ray& r, int n_bounces=10);
-    glm::vec4 trace_path(Ray& r, int n_bounces=10);
+    glm::vec4 trace_ray(Ray& r, int n_bounces = 10);
+    glm::vec4 trace_path(Ray& r, int n_bounces = 10);
 };
