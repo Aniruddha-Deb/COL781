d39e86e8d5a196e3802bb595020458f1b483bb73
Aniruddha Deb <aniruddha.deb.2002@gmail.com>
2024-04-19 16:02:38 +0530

    Tried some smoothing for denoising

--- a3_cpp/src/renderer.cpp
+++ a3_cpp/src/renderer.cpp
@@ -36,7 +36,11 @@ Renderer::~Renderer()
 
 void Renderer::render()
 {
+    int n_samples_pt = 100;
+    if (path_traced && curr_sample_no == n_samples_pt) return;
+    std::cout << curr_sample_no << std::endl;
     SDL_LockSurface(framebuffer);
+    float alpha = 0.8;
     Uint32 *pixels = (Uint32 *)framebuffer->pixels;
     SDL_PixelFormat *format = framebuffer->format;
     for (int px = 0; px < win.w; px++)
@@ -48,6 +52,22 @@ void Renderer::render()
             glm::vec3 pxcolor;
             if (path_traced)
             {
+                // get variance
+                // glm::vec2 v(0.f, 0.f);
+                // int n = 0;
+                // int rad = 1;
+                // for (int xp = -rad; xp <= rad; xp++) {
+                //     for (int yp = -rad; yp <= rad; yp++) {
+                //         if (px+xp < 0 || px+xp > win.w || py+yp < 0 || py+yp > win.h) continue;
+                //         glm::vec3 pxl = samplebuffer[(py+yp)*win.w + (px+xp)];
+                //         float lum = 0.299*pxl.r  + 0.587*pxl.g + 0.114*pxl.b;
+                //         v += glm::vec2(lum, lum*lum);
+                //         n++;
+                //     }
+                // }
+                // v /= n;
+                // float var = glm::max(0.f, v.y - v.x*v.x);
+                // if (curr_sample_no > 5 && var < 0.005) continue;
                 if (curr_sample_no > 1) {
                     r = scene.generate_ray(px, py, true);
                 }
@@ -64,6 +84,56 @@ void Renderer::render()
                 pxcolor = tone_map(scene.trace_ray(r));
             }
             pixels[py * win.w + px] = vec3_to_color(format, pxcolor);
+        }
+    }
+    if (path_traced && curr_sample_no == n_samples_pt-1) {
+        // A trous transform for smoothing
+        // hmm why is it not working, or is it working?
+        std::cout << "Smoothed at " << curr_sample_no << std::endl;
+        int lin_fsize = 3;
+        float lin_filter[3*3] = {
+            1.f/16, 1.f/8, 1.f/16,
+            1.f/8,  1.f/4, 1.f/8,
+            1.f/16, 1.f/8, 1.f/16
+        };
+        int bspl_fsize = 5;
+        float bspl_filter[5*5] = {
+            1.f/256, 1.f/64, 3.f/128, 1.f/64, 1.f/256,
+            1.f/64,  1.f/16, 3.f/32,  1.f/16, 1.f/64,
+            3.f/128, 3.f/32, 9.f/64,  3.f/32, 3.f/128,
+            1.f/64,  1.f/16, 3.f/32,  1.f/16, 1.f/64,
+            1.f/256, 1.f/64, 3.f/128, 1.f/64, 1.f/256
+        };
+
+        std::vector<glm::vec3> tbuf(samplebuffer.size());
+
+        int fsize = bspl_fsize;
+        float *filter = bspl_filter;
+        for (int it=0; it<3; it++) {
+            int incr = (1<<it);
+            for (int px=0; px < win.w; px++) {
+                #pragma omp parallel for
+                for (int py = 0; py < win.h; py++) {
+                    glm::vec3 filt_pval(0.f, 0.f, 0.f);
+                    for (int i=-(fsize/2); i<=(fsize/2); i++) {
+                        for (int j=-(fsize/2); j<=(fsize/2); j++) {
+                            if (px+i*incr < 0 || py+j*incr < 0 || px+i*incr >= win.w || py+j*incr >= win.h) continue;
+                            glm::vec3 pixval = samplebuffer[(py+j*incr)*win.w + (px+i*incr)];
+                            filt_pval += pixval*filter[(i+fsize/2)*fsize + (j+fsize/2)];
+                        }
+                    }
+                    tbuf[py*win.w + px] = filt_pval;
+                }
+            }
+            samplebuffer = tbuf;
+        }
+        for (int px = 0; px < win.w; px++)
+        {
+            #pragma omp parallel for
+            for (int py = 0; py < win.h; py++)
+            {
+                pixels[py * win.w + px] = vec3_to_color(format, tone_map(samplebuffer[py*win.w + px]));
+            }
         }
     }
     SDL_UnlockSurface(framebuffer);
